3333ea9ff9a8179f08b91339524cd4c768715bfd
grovas <arczir@gmail.com>
2017-05-22 18:29:15 +0200

    obsluga nowa gra

--- src/main/java/pl/sdacademy/TicTacToeServlet.java
+++ src/main/java/pl/sdacademy/TicTacToeServlet.java
@@ -16,6 +16,9 @@ public class TicTacToeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         HttpSession session = req.getSession(true);
+        if (req.getParameter("nowagra") != null){
+            session.removeAttribute("board");
+        }
         TicTacToe ticTacToe = (TicTacToe) session.getAttribute("board");
         if (ticTacToe == null) {
             ticTacToe = new TicTacToe();
@@ -31,14 +34,14 @@ public class TicTacToeServlet extends HttpServlet {
         }
 
         PrintWriter out = resp.getWriter();
-        out.println(renderBoard(ticTacToe));
+        out.println("<a href=\"nacisnijpole?nowagra=true\">Nowa gra</a><br />" + renderBoard(ticTacToe));
     }
 
     private String renderBoard (TicTacToe ticTacToe) {
        return renderField(0, 0, ticTacToe) + "|" + renderField(1, 0, ticTacToe) + "|" + renderField(2, 0, ticTacToe) + "<br />"
-        + "------<br />" +
+        + "-----<br />" +
                renderField(0, 1, ticTacToe) + "|" + renderField(1, 1, ticTacToe) + "|" + renderField(2, 1, ticTacToe) + "<br />"
-        + "-------<br />" +
+        + "-----<br />" +
                renderField(0, 2, ticTacToe) + "|" + renderField(1, 2, ticTacToe) + "|" + renderField(2, 2, ticTacToe) + "<br />";
 
     }
